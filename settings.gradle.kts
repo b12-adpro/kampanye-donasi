7976143341cc71c78fc4e7cd05c057217bce7193
favianzhafif26 <favianzhafif26@gmail.com>
2025-04-09 11:25:41 +0700

    Initialize SpringBoot

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "TK_ADPRO"
---
+++ src/main/java/id/ac/ui/cs/advprog/tk_adpro/TkAdproApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.tk_adpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkAdproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TkAdproApplication.class, args);
+    }
+
+}
