4012077d6229202f7ae2f73a98e540a6c0331af1
MaulanaSeto <maulana.seto@ui.ac.id>
2025-04-11 09:18:55 +0700

    [GREEN] Add test for main method

--- src/test/java/id/ac/ui/cs/advprog/tk_adpro/TkAdproApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/tk_adpro/TkAdproApplicationTests.java
@@ -1,13 +1,23 @@
 package id.ac.ui.cs.advprog.tk_adpro;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 @SpringBootTest
 class TkAdproApplicationTests {
-
     @Test
-    void contextLoads() {
-    }
+    void testMainMethod() {
+        try (MockedStatic<SpringApplication> mockedSpringApplication = Mockito.mockStatic(SpringApplication.class)) {
+            mockedSpringApplication.when(() -> SpringApplication.run(
+                TkAdproApplication.class, new String[]{}
+            )).thenReturn(null);
 
+            TkAdproApplication.main(new String[]{});
+
+            mockedSpringApplication.verify(() -> SpringApplication.run(TkAdproApplication.class, new String[]{}));
+        }
+    }
 }
