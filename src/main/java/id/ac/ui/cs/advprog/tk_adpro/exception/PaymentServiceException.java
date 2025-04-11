8aca0135c900dc9eb2ff65d46d2b94fc54d85c88
MaulanaSeto <maulana.seto@ui.ac.id>
2025-04-11 20:13:52 +0700

    [GREEN] Implement PaymentServiceException class

---
+++ src/main/java/id/ac/ui/cs/advprog/tk_adpro/exception/PaymentServiceException.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.advprog.tk_adpro.exception;
+
+public class PaymentServiceException extends RuntimeException {
+    public PaymentServiceException(String message) {
+        super(message);
+    }
+}
